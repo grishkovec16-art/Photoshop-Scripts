ad0ce520b56e1136fd1d77ec282fb556b1c15aea
grishkovec16-art <grishkovec16@gmail.com>
2026-02-02 11:23:15 +0300

    Move_Photos.jsx

--- Move_Photos.jsx
+++ Move_Photos.jsx
@@ -1,6 +1,29 @@
 #target photoshop
 
+/**
+ * Функция получения ключа из памяти Photoshop, 
+ * который туда записывает ваша панель (index.html)
+ */
+function getStoredLicenseKey() {
+    try {
+        var ref = new ActionReference();
+        ref.putProperty(charIDToTypeID('Prpt'), stringIDToTypeID('vignette_license_key'));
+        ref.putEnumerated(charIDToTypeID('capp'), charIDToTypeID('Ordn'), charIDToTypeID('Trgt'));
+        var result = executeActionGet(ref);
+        return result.getString(stringIDToTypeID('vignette_license_key'));
+    } catch (e) { 
+        return null; 
+    }
+}
+
 function flattenPhotosFolder() {
+    // --- ПРОВЕРКА ЗАЩИТЫ ---
+    var key = getStoredLicenseKey();
+    if (!key) {
+        alert("ОШИБКА: Плагин не активирован! Пожалуйста, введите ключ в панели.");
+        return;
+    }
+
     // Выбор корневой папки (например, "Обложка" или "сюжет и портреты")
     var rootFolder = Folder.selectDialog("Выберите папку, в которой лежат папки учеников");
     
@@ -8,6 +31,8 @@ function flattenPhotosFolder() {
 
     // Получаем все подпапки (папки учеников)
     var studentFolders = rootFolder.getFiles();
+
+    var count = 0;
 
     for (var i = 0; i < studentFolders.length; i++) {
         if (studentFolders[i] instanceof Folder) {
@@ -24,18 +49,21 @@ function flattenPhotosFolder() {
                         var destinationPath = studentFolders[i].fsName + "/" + files[j].name;
                         
                         // Перемещаем файл
-                        files[j].copy(destinationPath);
-                        files[j].remove();
+                        if (files[j].copy(destinationPath)) {
+                            files[j].remove();
+                        }
                     }
                 }
                 
                 // Удаляем теперь уже пустую папку "Photos"
                 photosFolder.remove();
+                count++;
             }
         }
     }
     
-    alert("Готово! Папки Photos обработаны.");
+    alert("Готово! Обработано папок: " + count);
 }
 
+// Запуск функции
 flattenPhotosFolder();
